20c9f4f957eec23de7298823803b20bb953f2fc9
ornequarra <orne_sq31@hotmail.com>
2023-08-20 19:47:57 -0300

    Subiendo main del req 1: contar total de tickets

--- internal/tickets/tickets.go
+++ internal/tickets/tickets.go
@@ -1,24 +1,27 @@
 package tickets
 
 import (
-	"time"
-	"github.com/ornequarra/Desafio1_Go/internal/tickets"
+	"os"
+	"strings"
 )
 
+// Ticket es una estructura que define todos los tickets de la aerolinea
 type Ticket struct {
-	id string
-	nombre string
-	email string
-	paisDestino string 
-	horaVuelo string
-	precio string
+	id          string
+	nombre      string
+	email       string
+	paisDestino string
+	horaVuelo   string
+	precio      string
 }
 
+// Storage es una estructura que almacena todos los datos de tickets
 type Storage struct {
-	Ticket []Ticket
+	Tickets []Ticket
 }
 
-func readFile(filename string) []tickets.Ticket{
+// readFile es una funcion que lee el archivo tickets.csv
+func ReadFile(filename string) []Ticket {
 
 	file, err := os.ReadFile(filename)
 
@@ -28,17 +31,17 @@ func readFile(filename string) []tickets.Ticket{
 
 	data := strings.Split(string(file), "\n")
 
-	var resultado []tickets.Ticket 
-	for i := 0; i< len(data); i++ {
-		if len(data[i]>0){
+	var resultado []Ticket
+	for i := 0; i < len(data); i++ {
+		if len(data[i]) > 0 {
 			file := strings.Split(string(data[i]), ",")
-			tickets := tickets.Ticket{
-				id file[0],
-				nombre file[1],
-				email file[2],
-				paisDestino file[3], 
-				horaVuelo file[4],
-				precio file[5],
+			tickets := Ticket{
+				id:          file[0],
+				nombre:      file[1],
+				email:       file[2],
+				paisDestino: file[3],
+				horaVuelo:   file[4],
+				precio:      file[5],
 			}
 			resultado = append(resultado, tickets)
 		}
@@ -46,11 +49,11 @@ func readFile(filename string) []tickets.Ticket{
 	return resultado
 }
 
-// ejemplo 1
-func GetTotalTickets(destination string) (int, error) {}
+// Requerimiento 1
+func (s Storage) GetTotalTickets(destination string) (int, error) {}
 
-// ejemplo 2
+// Requerimiento 2
 func GetMornings(time string) (int, error) {}
 
-// ejemplo 3
+// Requerimiento 3
 func AverageDestination(destination string, total int) (int, error) {}
--- main.go
+++ main.go
@@ -1,6 +1,10 @@
 package main
 
 import (
+	"fmt"
+	"log"
+	"os"
+
 	"github.com/ornequarra/Desafio1_Go/internal/tickets"
 )
 
@@ -8,5 +12,55 @@ const (
 	filename = "./tickets.csv"
 )
 
-func main() {}
+func main() {
+	defer func() {
+		if err := recover(); err != nil {
+			log.Fatal(err)
+		}
+	}()
 
+	storage := tickets.Storage{
+		Tickets: tickets.ReadFile(filename),
+	}
+
+	//Crear Canales para comunicarnos con las GoRoutines
+	canalTotalTickets := make(chan int)
+	defer close(canalTotalTickets)
+
+	canalTicket := make(chan tickets.Ticket)
+	defer close(canalTicket)
+
+	canalErr := make(chan error)
+	defer close(canalErr)
+
+	//Requerimiento 1: Contar total de tickets
+	var entrada string
+
+	fmt.Print("Ingrese pais a buscar :")
+	_, err := fmt.Scan(&entrada)
+
+	if err != nil {
+		log.Fatal(err)
+		os.Exit(1)
+	}
+
+	go func(chan int, chan error) {
+		totalTickets, err := storage.GetTotalTickets(entrada)
+		if err != nil {
+			canalErr <- err
+			return
+		}
+		canalTotalTickets <- totalTickets
+	}(canalTotalTickets, canalErr)
+
+	//Impresion de Canales
+	select {
+	case totalTicket := <-canalTotalTickets:
+		fmt.Println("El total de tickets para el pais %s es: %d", entrada, totalTicket)
+	case ticket := <-canalTicket:
+		fmt.Println(ticket)
+	case err := <-canalErr:
+		fmt.Println(err)
+		os.Exit(1)
+	}
+}
